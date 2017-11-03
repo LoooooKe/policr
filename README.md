61470a7c0a58e534a4ab55e88ce294480c8ee665
Lukas Feuz <feuzl1@bluewin.ch>
2017-11-03 17:42:41 +0100

    removed unused stuff

--- README.md
+++
@@ -1,4 +0,0 @@
-## tf-parse
-Since Terraform does not currently support outputting data via plan or show as a consumable data structure this little lib does just that.
-
-_Examples coming..._
--- test.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-// const debug = require('debug')('test');
-const State = require('./lib').State;
-const state = new State();
-
-console.log( JSON.stringify( state.parse( require('fs').readFileSync( './test/data/apply/db/terraform.tfstate', 'utf8' ) ), null, 2 ) );
-
-process.exit(0);
-
